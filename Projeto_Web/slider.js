cc1f4dfde94ca94c1d5b8ffd1054e35998917976
Joan Brando <joanbrando50@gmail.com>
2024-03-25 14:52:14 -0300

    Modificação do slider com edição de imagens em css e redimensionamento. Inclusão de novas imagens, um novo java script, alterações iniciais no desenvolvimento da página.

---
+++ Projeto_Web/slider.js
@@ -0,0 +1,45 @@
+let nextButton = document.getElementById('next');
+let prevButton = document.getElementById('prev');
+let backButton = document.getElementById('back');
+let carrossel = document.querySelector('.carrossel');
+let listHTML = document.querySelector('.carrossel .list');
+let seeMoreButtons = document.querySelectorAll('.seeMore');
+
+nextButton.onclick = function(){
+    showSlider('next');
+}
+prevButton.onclick = function(){
+    showSlider('prev');
+}
+
+let unAcceptClick;
+const showSlider = (type) => {
+    nextButton.style.pointerEvents = 'none';
+    prevButton.style.pointerEvents = 'none';
+
+    carrossel.classList.remove('prev', 'next');
+    let items = document.querySelectorAll('.carrossel .list .item');
+    if(type === 'next'){
+        listHTML.appendChild(items[0]);
+        carrossel.classList.add('next');
+    }else{
+        let positionLast = items.length -1;
+        listHTML.prepend(items[positionLast]);
+        carrossel.classList.add('prev');
+    }
+
+    clearTimeout(unAcceptClick);
+    unAcceptClick = setTimeout(() => {
+        nextButton.style.pointerEvents = 'auto';
+        prevButton.style.pointerEvents = 'auto';
+    }, 1000);
+}
+
+seeMoreButtons.forEach(button => {
+    button.onclick = function(){
+        carrossel.classList.add('showDetail');
+    }
+})
+backButton.onclick = function(){
+    carrossel.classList.remove('showDetail');
+}
